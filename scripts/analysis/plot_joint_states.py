0e0ba4f3a1bec37de09e194d39d877b59ea82130
Adam Heins <mail@adamheins.com>
2023-01-30 16:09:43 -0500

    WIP: add commanded UR10 velocities to plotting script.

--- scripts/analysis/plot_joint_states.py
+++ scripts/analysis/plot_joint_states.py
@@ -17,6 +17,15 @@ def main():
     ur10_msgs = [msg for _, msg, _ in bag.read_messages("/ur10/joint_states")]
     ridgeback_msgs = [msg for _, msg, _ in bag.read_messages("/ridgeback/joint_states")]
 
+    ur10_cmd_msgs = [msg for _, msg, _ in bag.read_messages("/ur10/cmd_vel")]
+    ur10_cmd_ts = np.array([t.to_sec() for _, _, t in bag.read_messages("/ur10/cmd_vel")])
+    ur10_cmd_ts -= ur10_cmd_ts[0]
+
+    ur10_cmd_vels = []
+    for msg in ur10_cmd_msgs:
+        ur10_cmd_vels.append(msg.data)
+    ur10_cmd_vels = np.array(ur10_cmd_vels)
+
     tas, qas, vas = ros_utils.parse_ur10_joint_state_msgs(ur10_msgs)
     tbs, qbs, vbs = ros_utils.parse_ridgeback_joint_state_msgs(ridgeback_msgs)
 
@@ -51,8 +60,17 @@ def main():
 
     plt.figure()
     for i in range(6):
-        plt.plot(tas, vas[:, i], label=f"θ_{i+1}")
+        plt.plot(tas, vas[:, i], label=f"v_{i+1}")
     plt.title("UR10 Joint Velocities")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Joint velocity (rad/s)")
+    plt.legend()
+    plt.grid()
+
+    plt.figure()
+    for i in range(6):
+        plt.plot(ur10_cmd_ts, ur10_cmd_vels[:, i], label=f"vc_{i+1}")
+    plt.title("UR10 Commanded Joint Velocities")
     plt.xlabel("Time (s)")
     plt.ylabel("Joint velocity (rad/s)")
     plt.legend()
