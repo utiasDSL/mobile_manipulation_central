b1f2e95dd27a37024cbbf706e3b8360e770789ff
Adam Heins <mail@adamheins.com>
2025-07-23 11:52:00 -0400

    Add load_pkg_config convenience function.

--- src/mobile_manipulation_central/utils.py
+++ src/mobile_manipulation_central/utils.py
@@ -24,3 +24,25 @@ def load_home_position(name="default", path=None):
     with open(path) as f:
         data = yaml.safe_load(f)
     return np.array(data[name])
+
+
+def load_pkg_config(pkg_name, relpath):
+    """Load a YAML config file from a ROS package.
+
+    Parameters
+    ----------
+    pkg_name : str
+        The name of the ROS package.
+    relpath : str or Path
+        Path of the config file relative to the package root.
+
+    Returns
+    -------
+    : dict
+        The configuration.
+    """
+    rospack = rospkg.RosPack()
+    pkg_path = Path(rospack.get_path(pkg_name))
+    path = pkg_path / relpath
+    with open(path) as f:
+        return yaml.safe_load(f)
